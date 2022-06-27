8ce8c4ca3fc9eb2ab73789d1a60de6b1cc8d788b
Tianshu Xiao <tx2@g.ucla.edu>
2022-06-27 15:34:59 -0700

    second

--- lib/enumerables.rb
+++ lib/enumerables.rb
@@ -29,6 +29,36 @@ class Array
         return selected
     end
 
-    
+    def my_any?(&proc)
+        accpected = false
+        self.my_each do |ele|
+            accpected = true if proc.call(ele)
+        end
+        accpected
+    end
+
+    def my_all?(&proc)
+        accpected = true
+        self.my_each do |ele|
+            accpected = false if !proc.call(ele)
+        end
+        accpected
+    end
+
+    def my_flatten
+        if !self.is_a?(Array)
+            return [self]
+        end
+
+        res = []
+        self.each do |el|
+            if el.is_a?(Array)
+                res += el.my_flatten
+            else
+                res << el
+            end
+        end
+        res
+    end
 
 end
