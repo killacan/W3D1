574feb38a8fec3e6cef4662b4d0cd4687a4cc22e
Killacan <kameron000@yahoo.com>
2022-06-27 17:10:32 -0700

    third switch off

--- lib/enumerables.rb
+++ lib/enumerables.rb
@@ -64,18 +64,59 @@ class Array
     # create zip, contains a lot of sub arrays. Put self into first sub array, 
     # and additional arguments we put in the rest of the sub array. 
 
+
+    #[[stuff][stuff]] unshift([self]) [[self][stuff][otherstuff]].transpose
     # arrs.flatten(1) 
+
+    # [[1, 2], [4,5,6], [7,8,9]]
+
+    # [[1, 2][ 4, 5, 6 ][ 7, 8, 9 ]]
+    # [[nil, nil, nil][nil, nil nil]]
+
+    # if one of the sub_arrays has a length that is longer than self, then index into a j that
+    # does not exist. do a (0 ... self.length).each do |num|
+
     def my_zip(*arrs)
-        zip = Array.new(self.length) {Array.new(array.length, nil)}
-        zip[0] = self
-        (1 .. self.length - 1).each do |index|
-            zip[index] = 
-        end
+        zip = Array.new(self.length) {Array.new(arrs.length + 1, nil)}
+        array = arrs.unshift(self)
 
-        zip.each do |sub_array|
-            arrs.each do |ele| 
-
+        array.each.with_index do |sub_array, i| 
+            (0...self.length).each do |j| 
+                zip[j][i] = sub_array[j]
             end
         end
+        return zip
+    end
+
+    def my_rotate(num=1)
+        rotated = []
+        index = num % self.length
+
+        rotated += self[index.. -1] 
+        rotated += self[0 ... index]
+        return rotated
+    end
+
+    def my_join(char="")
+        str = "" 
+
+        self.each do |el| 
+            str += (el + char)
+        end
+        if char == ""
+            return str
+        else
+            return str[0 ... - 1]
+        end
+    end
+
+    def my_reverse
+        arr = []
+
+        self.each do |el|
+            arr.unshift(el)
+        end
+        return arr
     end
 end
+
